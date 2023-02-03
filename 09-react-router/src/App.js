ffb4b5ab346d5564e282239b799f4d8833815561
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-03 15:52:14 -0300

    layouts and nested routes

--- 09-react-router/src/App.js
+++ 09-react-router/src/App.js
@@ -1,14 +1,21 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './pages/home';
-import Products from './pages/products';
+import HomePage from './pages/Home';
+import Products from './pages/Products';
+import RootLayout from './pages/Root';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <Products /> }
+	{
+		path: '/',
+		element: <RootLayout />,
+		children: [
+			{ path: '/', element: <HomePage /> },
+			{ path: '/products', element: <Products /> },
+		],
+	},
 ]);
 
 function App() {
-	return <RouterProvider router={router}/>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
