c7f390dace3b1a265c13d13fb7c01ac56268e6f3
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-03 15:11:18 -0300

    navigate between pages via links

--- 09-react-router/src/pages/products.js
+++ 09-react-router/src/pages/products.js
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Products = () => {
- return (<h1>My Products</h1>);
-}
+	return (
+		<>
+			<h1>Products Page</h1>
+			<p>
+				Go back to <Link to="/">Homepage</Link>.
+			</p>
+		</>
+	);
+};
 
 export default Products;
