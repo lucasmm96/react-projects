fb489b7d2d4c1a99055347062ad2f723759ac647
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-03 14:53:16 -0300

    defining routes

--- 09-react-router/src/App.js
+++ 09-react-router/src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/home';
+import Products from './pages/products';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <Products /> }
+]);
+
 function App() {
-  return <div></div>;
+	return <RouterProvider router={router}/>;
 }
 
 export default App;
