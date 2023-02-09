8575d3fa186245f4aadd56f87d7405c8208be7b9
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-09 17:21:41 -0300

    where should loader code be stored

--- 10-react-router-2/frontend/src/App.js
+++ 10-react-router-2/frontend/src/App.js
@@ -1,29 +1,7 @@
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Events from './pages/Events';
+import Events, { loader as eventsLoader } from './pages/Events';
 import EventDetail from './pages/EventDetail';
 import NewEvent from './pages/NewEvent';
 import EditEvent from './pages/EditEvent';
@@ -41,19 +19,7 @@ function App() {
 					path: 'events',
 					element: <EventsRootLayout />,
 					children: [
-						{
-							index: true,
-							element: <Events />,
-							loader: async () => {
-								const response = await fetch('http://localhost:8080/events');
-								if (!response.ok) {
-									// ...
-								} else {
-									const resData = await response.json();
-									return resData.events;
-								}
-							},
-						},
+						{ index: true, element: <Events />, loader: eventsLoader },
 						{ path: ':eventId', element: <EventDetail /> },
 						{ path: 'new', element: <NewEvent /> },
 						{ path: ':eventId/edit', element: <EditEvent /> },
