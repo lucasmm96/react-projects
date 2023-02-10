68366c4396042f88417b46304392f8a339b02cf0
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-10 13:29:16 -0300

    extracting error data & throwing responses

--- 10-react-router-2/frontend/src/pages/Error.js
+++ 10-react-router-2/frontend/src/pages/Error.js
@@ -1,5 +1,30 @@
+import { useRouteError } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
+import PageContent from '../components/PageContent';
+
 const Error = () => {
-	return <h1>An error ocurred!</h1>;
+	const error = useRouteError();
+
+	let title = 'An error ocurred!';
+	let message = 'Something went wrong!';
+
+	if (error.status === 500) {
+		message = JSON.parse(error.data).message;
+	}
+
+	if (error.status === 404) {
+		title = 'Not found!';
+		message = 'Could not find the resource or page.';
+	}
+
+	return (
+		<>
+			<MainNavigation />
+			<PageContent title={title}>
+				<p>{message}</p>
+			</PageContent>
+		</>
+	);
 };
 
 export default Error;
--- 10-react-router-2/frontend/src/pages/Events.js
+++ 10-react-router-2/frontend/src/pages/Events.js
@@ -5,7 +5,7 @@ import EventsList from '../components/EventsList';
 const EventsPage = () => {
 	const data = useLoaderData();
 	const events = data.events;
-	
+
 	return (
 		<>
 			<EventsList events={events} />
@@ -18,7 +18,9 @@ export default EventsPage;
 export async function loader() {
 	const response = await fetch('http://localhost:8080/eventss');
 	if (!response.ok) {
-		throw { message: 'Could not fetch events.' }
+		throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+			status: 500,
+		});
 	} else {
 		return response;
 	}
