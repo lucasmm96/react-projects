3601e1d1e3fa523dfdfec1c906fa8cbbe00df469
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-10 13:32:41 -0300

    the json() utility function

--- 10-react-router-2/frontend/src/pages/Events.js
+++ 10-react-router-2/frontend/src/pages/Events.js
@@ -1,26 +1,19 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, json } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
 const EventsPage = () => {
 	const data = useLoaderData();
 	const events = data.events;
-
-	return (
-		<>
-			<EventsList events={events} />
-		</>
-	);
+	return <EventsList events={events} />;
 };
 
 export default EventsPage;
 
 export async function loader() {
-	const response = await fetch('http://localhost:8080/events');
+	const response = await fetch('http://localhost:8080/eventss');
 	if (!response.ok) {
-		throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
-			status: 500,
-		});
+		return json({ message: 'Could not fetch events.' }, { status: 500 });
 	} else {
 		return response;
 	}
