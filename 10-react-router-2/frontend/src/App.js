65545c130f64b9e029d74bb4c1952dfa9ecf4668
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-14 16:27:54 -0300

    The useRouteLoaderData() Hook & Accessing Data from Other Routes

--- 10-react-router-2/frontend/src/App.js
+++ 10-react-router-2/frontend/src/App.js
@@ -24,11 +24,17 @@ function App() {
 						{ index: true, element: <Events />, loader: eventsLoader },
 						{
 							path: ':eventId',
-							element: <EventDetail />,
+							id: 'event-detail',
 							loader: eventDetailLoader,
+							children: [
+								{
+									index: true,
+									element: <EventDetail />,
+								},
+								{ path: 'edit', element: <EditEvent /> },
+							],
 						},
 						{ path: 'new', element: <NewEvent /> },
-						{ path: ':eventId/edit', element: <EditEvent /> },
 					],
 				},
 			],
