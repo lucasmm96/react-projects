2978cca1b0bd7d9ec990d1fec5db74ed7d45c13a
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-15 16:11:49 -0300

    useFetcher

--- 10-react-router-2/frontend/src/App.js
+++ 10-react-router-2/frontend/src/App.js
@@ -1,59 +1,69 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Error from './pages/Error';
-import Home from './pages/Home';
-import Events, { loader as eventsLoader } from './pages/Events';
-import EventDetail, {
+import EditEventPage from './pages/EditEvent';
+import ErrorPage from './pages/Error';
+import EventDetailPage, {
 	loader as eventDetailLoader,
 	action as deleteEventAction,
 } from './pages/EventDetail';
-import NewEvent from './pages/NewEvent';
-import EditEvent from './pages/EditEvent';
-import RootLayout from './pages/Root';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventsRootLayout from './pages/EventsRoot';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <RootLayout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{ index: true, element: <HomePage /> },
+			{
+				path: 'events',
+				element: <EventsRootLayout />,
+				children: [
+					{
+						index: true,
+						element: <EventsPage />,
+						loader: eventsLoader,
+					},
+					{
+						path: ':eventId',
+						id: 'event-detail',
+						loader: eventDetailLoader,
+						children: [
+							{
+								index: true,
+								element: <EventDetailPage />,
+								action: deleteEventAction,
+							},
+							{
+								path: 'edit',
+								element: <EditEventPage />,
+								action: manipulateEventAction,
+							},
+						],
+					},
+					{
+						path: 'new',
+						element: <NewEventPage />,
+						action: manipulateEventAction,
+					},
+				],
+			},
+			{
+				path: 'newsletter',
+				element: <NewsletterPage />,
+				action: newsletterAction,
+			},
+		],
+	},
+]);
 
 function App() {
-	const router = createBrowserRouter([
-		{
-			path: '/',
-			element: <RootLayout />,
-			errorElement: <Error />,
-			children: [
-				{ index: true, element: <Home /> },
-				{
-					path: 'events',
-					element: <EventsRootLayout />,
-					children: [
-						{ index: true, element: <Events />, loader: eventsLoader },
-						{
-							path: ':eventId',
-							id: 'event-detail',
-							loader: eventDetailLoader,
-							children: [
-								{
-									index: true,
-									element: <EventDetail />,
-									action: deleteEventAction,
-								},
-								{
-									path: 'edit',
-									element: <EditEvent />,
-									action: manipulateEventAction,
-								},
-							],
-						},
-						{
-							path: 'new',
-							element: <NewEvent />,
-							action: manipulateEventAction,
-						},
-					],
-				},
-			],
-		},
-	]);
-
 	return <RouterProvider router={router} />;
 }
 
