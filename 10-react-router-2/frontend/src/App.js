f35d104cc7e3768ae6edb7cc49fb80752c8b2dd3
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-15 14:44:05 -0300

    submit data programmatically

--- 10-react-router-2/frontend/src/App.js
+++ 10-react-router-2/frontend/src/App.js
@@ -3,7 +3,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Error from './pages/Error';
 import Home from './pages/Home';
 import Events, { loader as eventsLoader } from './pages/Events';
-import EventDetail, { loader as eventDetailLoader } from './pages/EventDetail';
+import EventDetail, {
+	loader as eventDetailLoader,
+	action as deleteEventAction,
+} from './pages/EventDetail';
 import NewEvent, { action as newEventAction } from './pages/NewEvent';
 import EditEvent from './pages/EditEvent';
 import RootLayout from './pages/Root';
@@ -30,8 +33,12 @@ function App() {
 								{
 									index: true,
 									element: <EventDetail />,
+									action: deleteEventAction,
 								},
-								{ path: 'edit', element: <EditEvent /> },
+								{
+									path: 'edit',
+									element: <EditEvent />,
+								},
 							],
 						},
 						{ path: 'new', element: <NewEvent />, action: newEventAction },
