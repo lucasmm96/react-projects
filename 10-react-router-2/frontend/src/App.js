b45a5145b533c814f97cdde1b9415a279dde4f7d
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-15 15:53:08 -0300

    reusing actions via request methods

--- 10-react-router-2/frontend/src/App.js
+++ 10-react-router-2/frontend/src/App.js
@@ -7,10 +7,11 @@ import EventDetail, {
 	loader as eventDetailLoader,
 	action as deleteEventAction,
 } from './pages/EventDetail';
-import NewEvent, { action as newEventAction } from './pages/NewEvent';
+import NewEvent from './pages/NewEvent';
 import EditEvent from './pages/EditEvent';
 import RootLayout from './pages/Root';
 import EventsRootLayout from './pages/EventsRoot';
+import { action as manipulateEventAction } from './components/EventForm';
 
 function App() {
 	const router = createBrowserRouter([
@@ -38,10 +39,15 @@ function App() {
 								{
 									path: 'edit',
 									element: <EditEvent />,
+									action: manipulateEventAction,
 								},
 							],
 						},
-						{ path: 'new', element: <NewEvent />, action: newEventAction },
+						{
+							path: 'new',
+							element: <NewEvent />,
+							action: manipulateEventAction,
+						},
 					],
 				},
 			],
