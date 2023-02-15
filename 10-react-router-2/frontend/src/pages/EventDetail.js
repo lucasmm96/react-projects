2178d26eb271e88ca86c1457357d9cb5e1511bf1
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-15 16:53:04 -0300

    Controlling Which Data Should Be Deferred

--- 10-react-router-2/frontend/src/pages/EventDetail.js
+++ 10-react-router-2/frontend/src/pages/EventDetail.js
@@ -1,16 +1,35 @@
-import { useRouteLoaderData, json, redirect } from 'react-router-dom';
+import { Suspense } from 'react';
+import {
+	useRouteLoaderData,
+	json,
+	redirect,
+	defer,
+	Await,
+} from 'react-router-dom';
 import EventItem from '../components/EventItem';
+import EventsList from '../components/EventsList';
 
 const EventDetailPage = () => {
-	const data = useRouteLoaderData('event-detail');
-	return <EventItem event={data.event} />;
+	const { event, events } = useRouteLoaderData('event-detail');
+	return (
+		<>
+			<Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+				<Await resolve={event}>
+					{(loadedEvent) => <EventItem event={loadedEvent} />}
+				</Await>
+			</Suspense>
+			<Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+				<Await resolve={events}>
+					{(loadedEvents) => <EventsList event={loadedEvents} />}
+				</Await>
+			</Suspense>
+		</>
+	);
 };
 
 export default EventDetailPage;
 
-export async function loader({ request, params }) {
-	const id = params.eventId;
-
+export async function loadEvent(id) {
 	const response = await fetch('http://localhost:8080/events/' + id);
 
 	if (!response.ok) {
@@ -19,8 +38,26 @@ export async function loader({ request, params }) {
 			{ status: 500 }
 		);
 	} else {
-		return response;
+		const resData = await response.json();
+		return resData.event;
 	}
+}
+
+export async function loadEvents() {
+	const response = await fetch('http://localhost:8080/events');
+	if (!response.ok) {
+		return json({ message: 'Could not fetch events.' }, { status: 500 });
+	} else {
+		const resData = await response.json();
+		return resData.events;
+	}
+}
+
+export async function loader({ request, params }) {
+	return defer({
+		event: await loadEvent(params.eventId),
+		events: loadEvents(),
+	});
 }
 
 export async function action({ request, params }) {
--- 10-react-router-2/frontend/src/pages/Events.js
+++ 10-react-router-2/frontend/src/pages/Events.js
@@ -24,6 +24,7 @@ export async function loadEvents() {
 		return resData.events;
 	}
 }
+
 export function loader() {
 	return defer({
 		events: loadEvents(),
