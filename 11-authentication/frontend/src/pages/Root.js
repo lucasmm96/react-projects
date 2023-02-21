207bdfc99edf72ad8411eb654c2bf160f03df6a3
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-21 17:59:53 -0300

    managing the token expiration

--- 11-authentication/frontend/src/pages/Root.js
+++ 11-authentication/frontend/src/pages/Root.js
@@ -1,19 +1,39 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
-  // const navigation = useNavigation();
+	const token = useLoaderData('root');
+	const submit = useSubmit();
 
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+	useEffect(() => {
+		if (!token) {
+			return;
+		}
+
+		if (token === 'EXPIRED') {
+			submit(null, { action: '/logout', method: 'post' });
+			return;
+		}
+
+		const tokenDuration = getTokenDuration();
+
+		setTimeout(() => {
+			submit(null, { action: '/logout', method: 'post' });
+		}, tokenDuration);
+	}, [token, submit]);
+
+	return (
+		<>
+			<MainNavigation />
+			<main>
+				{/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+				<Outlet />
+			</main>
+		</>
+	);
 }
 
 export default RootLayout;
--- 11-authentication/frontend/src/util/auth.js
+++ 11-authentication/frontend/src/util/auth.js
@@ -1,7 +1,28 @@
 import { redirect } from 'react-router-dom';
 
+export function getTokenDuration() {
+	const storedExpiration = localStorage.getItem('expiration');
+	const expirationDate = new Date(storedExpiration);
+	const now = new Date();
+	return expirationDate.getTime() - now.getTime();
+}
+
 export function getAuthToken() {
-	return localStorage.getItem('token');
+	const token = localStorage.getItem('token');
+
+	if (!token) {
+		return null;
+	}
+
+	const tokenDuration = getTokenDuration();
+	
+	console.log('tokenDuration:', tokenDuration);
+
+	if (tokenDuration < 0) {
+		return 'EXPIRED';
+	}
+
+	return token;
 }
 
 export function authLoader() {
