f9f7f87cd841e9b052cb6dd96d5b47803d324e88
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-22 14:13:57 -0300

    outputting a list of meetups

--- 13-nextjs/pages/index.js
+++ 13-nextjs/pages/index.js
@@ -1,5 +1,31 @@
-function Home() {
-	return <h1>Home</h1>;
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUP = [
+	{
+		id: 'm1',
+		title: 'A first Meetup',
+		image:
+			'https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg',
+		address: 'Some address 1, 12345, Some City',
+	},
+	{
+		id: 'm2',
+		title: 'A second Meetup',
+		image:
+			'https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg',
+		address: 'Some address 2, 12345, Some City',
+	},
+	{
+		id: 'm3',
+		title: 'A third Meetup',
+		image:
+			'https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg',
+		address: 'Some address 3, 12345, Some City',
+	},
+];
+
+function HomePage() {
+	return <MeetupList meetups={DUMMY_MEETUP}></MeetupList>;
 }
 
-export default Home;
+export default HomePage;
