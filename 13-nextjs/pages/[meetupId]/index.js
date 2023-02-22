73c39255664ee70b97c8d2052ee8d82ebf02212e
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-22 16:12:31 -0300

    adding custom components & css modules

--- 13-nextjs/pages/[meetupId]/index.js
+++ 13-nextjs/pages/[meetupId]/index.js
@@ -1,5 +1,14 @@
-function Meetup() {
-	return <h1>Meetup</h1>;
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+	return (
+		<MeetupDetail
+			image="https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg"
+			title="First Meetup"
+			address="Some street 5, Some city 1234"
+			description="The meetup description"
+		/>
+	);
 }
 
-export default Meetup;
+export default MeetupDetails;
