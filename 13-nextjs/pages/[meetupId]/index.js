852708d5a90301c9e242a8211c6020a762ef6833
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-22 16:55:35 -0300

    preparing paths with 'getStaticPaths' & working with fallback pages

--- 13-nextjs/pages/[meetupId]/index.js
+++ 13-nextjs/pages/[meetupId]/index.js
@@ -1,6 +1,6 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
 	return (
 		<MeetupDetail
 			image="https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg"
@@ -9,6 +9,42 @@ function MeetupDetails() {
 			description="The meetup description"
 		/>
 	);
+}
+
+export async function getStaticPaths() {
+	return {
+		fallback: false,
+		paths: [
+			{
+				params: {
+					meetupId: 'm1',
+				},
+			},
+			{
+				params: {
+					meetupId: 'm2',
+				},
+			},
+		],
+	};
+}
+
+export async function getStaticProps(context) {
+	const meetupId = context.params.meetupId;
+
+	return {
+		props: {
+			meetupProp: {
+				id: meetupId,
+				image:
+					'https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg',
+				title: 'First Meetup',
+				address: 'Some street 5, Some city 1234',
+				description: 'The meetup description',
+			},
+		},
+		revalidate: 10,
+	};
 }
 
 export default MeetupDetails;
