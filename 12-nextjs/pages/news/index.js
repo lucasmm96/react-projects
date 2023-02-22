c0796a00d8b426b178fc8c7bd72f616ec8f06a29
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-22 13:51:42 -0300

    linking between pages

--- 12-nextjs/pages/news/index.js
+++ 12-nextjs/pages/news/index.js
@@ -1,5 +1,19 @@
+import Link from 'next/link';
+
 function NewsPage() {
-	return <h1>This is the News Page</h1>;
+	return (
+		<>
+			<h1>This is the News Page</h1>
+			<ul>
+				<li>
+					<Link href="/news/1234">Link 1234</Link>
+				</li>
+				<li>
+					<Link href="/news/567">Link 567</Link>
+				</li>
+			</ul>
+		</>
+	);
 }
 
 export default NewsPage;
