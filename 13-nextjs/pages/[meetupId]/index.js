f3bb258acab5d654d2c9897717c4eb404229b39e
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-23 15:37:34 -0300

    generating meetup details data & preparing  pages

--- 13-nextjs/pages/[meetupId]/index.js
+++ 13-nextjs/pages/[meetupId]/index.js
@@ -1,47 +1,62 @@
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
 function MeetupDetails(props) {
 	return (
 		<MeetupDetail
-			image="https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg"
-			title="First Meetup"
-			address="Some street 5, Some city 1234"
-			description="The meetup description"
+			image={props.meetupData.image}
+			title={props.meetupData.title}
+			address={props.meetupData.address}
+			description={props.meetupData.description}
 		/>
 	);
 }
 
 export async function getStaticPaths() {
+	dotenv.config();
+	const client = await MongoClient.connect(process.env.MONGO_URI);
+	const db = client.db();
+	const meetupsCollection = db.collection('meetups');
+
+	const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+	const data = meetups.map((meetup) => ({
+		params: { meetupId: meetup._id.toString() },
+	}));
+
+	client.close();
+
 	return {
 		fallback: false,
-		paths: [
-			{
-				params: {
-					meetupId: 'm1',
-				},
-			},
-			{
-				params: {
-					meetupId: 'm2',
-				},
-			},
-		],
+		paths: data,
 	};
 }
 
 export async function getStaticProps(context) {
 	const meetupId = context.params.meetupId;
 
+	dotenv.config();
+	const client = await MongoClient.connect(process.env.MONGO_URI);
+	const db = client.db();
+	const meetupsCollection = db.collection('meetups');
+
+	const selectedMeetup = await meetupsCollection.findOne({
+		_id: new ObjectId(meetupId),
+	});
+
+	const selectedData = {
+		id: selectedMeetup._id.toString(),
+		title: selectedMeetup.data.title,
+		address: selectedMeetup.data.address,
+		image: selectedMeetup.data.image,
+		description: selectedMeetup.data.description
+	};
+
+	client.close();
+
 	return {
 		props: {
-			meetupProp: {
-				id: meetupId,
-				image:
-					'https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg',
-				title: 'First Meetup',
-				address: 'Some street 5, Some city 1234',
-				description: 'The meetup description',
-			},
+			meetupData: selectedData,
 		},
 		revalidate: 10,
 	};
--- 13-nextjs/pages/index.js
+++ 13-nextjs/pages/index.js
@@ -1,29 +1,31 @@
-import { useEffect, useState } from 'react';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_MEETUP = [
-	{
-		id: 'm1',
-		title: 'A first Meetup',
-		image:
-			'https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg',
-		address: 'Some address 1, 12345, Some City',
-	},
-	{
-		id: 'm2',
-		title: 'A second Meetup',
-		image:
-			'https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg',
-		address: 'Some address 2, 12345, Some City',
-	},
-	{
-		id: 'm3',
-		title: 'A third Meetup',
-		image:
-			'https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg',
-		address: 'Some address 3, 12345, Some City',
-	},
-];
+// const DUMMY_MEETUP = [
+// 	{
+// 		id: 'm1',
+// 		title: 'A first Meetup',
+// 		image:
+// 			'https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg',
+// 		address: 'Some address 1, 12345, Some City',
+// 	},
+// 	{
+// 		id: 'm2',
+// 		title: 'A second Meetup',
+// 		image:
+// 			'https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg',
+// 		address: 'Some address 2, 12345, Some City',
+// 	},
+// 	{
+// 		id: 'm3',
+// 		title: 'A third Meetup',
+// 		image:
+// 			'https://www.freecodecamp.org/news/content/images/size/w2000/2019/08/meetup-talk.jpg',
+// 		address: 'Some address 3, 12345, Some City',
+// 	},
+// ];
 
 function HomePage(props) {
 	return <MeetupList meetups={props.meetups}></MeetupList>;
@@ -43,9 +45,24 @@ function HomePage(props) {
 
 // get data according to frequency defined (in seconds) on 'revalidate' field.
 export async function getStaticProps() {
+	dotenv.config();
+	const client = await MongoClient.connect(process.env.MONGO_URI);
+	const db = client.db();
+	const meetupsCollection = db.collection('meetups');
+
+	const meetups = await meetupsCollection.find().toArray();
+	const data = meetups.map((meetup) => ({
+		title: meetup.data.title,
+		address: meetup.data.address,
+		image: meetup.data.image,
+		id: meetup._id.toString(),
+	}));
+
+	client.close();
+
 	return {
 		props: {
-			meetups: DUMMY_MEETUP,
+			meetups: data,
 		},
 		revalidate: 10,
 	};
